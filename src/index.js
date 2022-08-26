4240aeb1f59240bfb7f8a5809d83c02ac4b93dc1
Gracelaura <gracenjuguna05@gmail.com>
2022-08-27 01:53:47 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querrySelector("form");
+
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querrySelector("input#searchById")
+
+    console.log(input.value)
+
+    fetch("http://localhost:3000/movies")
+    .then(response => response.json())
+    .then(data =>  {
+        const title = document.querrySelector("section#movieDetails h4")
+        const summary = document.querrySelector("section#movieDetails p")
+
+        title.innerText = data.title
+        summary.innerText = data.summary
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
